32c9a55b76d9ccd8872b80dcef2b1c45da971ec0
shamb0t <shams.methnani@gmail.com>
2018-08-31 17:33:54 +0400

    Add webpack + build script

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const path = require('path')
+const Uglify = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  entry: './index-browser.js',
+  output: {
+    libraryTarget: 'var',
+    library: 'Keystore',
+    filename: 'index-browser.min.js'
+  },
+  target: 'web',
+  devtool: 'sourcemap',
+  node: {
+    console: false,
+    Buffer: true
+  },
+  plugins: [
+    new Uglify(),
+  ],
+  resolve: {
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, '../node_modules')
+    ]
+  },
+  resolveLoader: {
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, '../node_modules')
+    ],
+    moduleExtensions: ['-loader']
+  },
+}
