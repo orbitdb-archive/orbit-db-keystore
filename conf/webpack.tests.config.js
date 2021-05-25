f5af86cc7034a98993c11c479f34f49a221b5f75
tabcat <andersb@tuta.io>
2021-05-25 12:30:43 -0500

    fix some linter errors

--- conf/webpack.tests.config.js
+++ conf/webpack.tests.config.js
@@ -19,7 +19,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
       }
     }),
     new webpack.IgnorePlugin(/mongo|redis/)
